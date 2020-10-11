2d15a225eed9fed0c6924c93bf7d48429d147eec
SaberCon <894876785@qq.com>
2020-10-11 12:52:41 +0800

    优化 413
    Signed-off-by: SaberCon <894876785@qq.com>

--- 401_500/413.py
+++ 401_500/413.py
@@ -1,15 +1,11 @@
 class Solution:
     def numberOfArithmeticSlices(self, A: List[int]) -> int:
-        length = len(A)
-        is_arithmetic = [True for _ in range(length)]
         ans = 0
-        for i in range(2, length):
-            last_ans = ans
-            for j in range(0, length - i):
-                if is_arithmetic[j] and A[j + 1] - A[j] == A[j + i] - A[j + i - 1]:
-                    ans += 1
-                else:
-                    is_arithmetic[j] = False
-            if last_ans == ans:
-                break
+        count = 0
+        for i in range(2, len(A)):
+            if A[i] - A[i - 1] == A[i - 1] - A[i - 2]:
+                count += 1
+                ans += count
+            else:
+                count = 0
         return ans
