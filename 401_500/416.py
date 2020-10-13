0a0add3f9f01402ceed24cd222bce7d8e626db32
SaberCon <894876785@qq.com>
2020-10-13 22:37:34 +0800

    416
    Signed-off-by: SaberCon <894876785@qq.com>

---
+++ 401_500/416.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        target = total // 2
+        sum_set = {0}
+        for num in nums:
+            sum_set = sum_set + {i + num for i in sum_set}
+            if target in sum_set:
+                return True
+        return False
