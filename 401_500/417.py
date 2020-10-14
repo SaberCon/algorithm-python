6670a1126b8a44dca9fa119c4684889b66602420
SaberCon <894876785@qq.com>
2020-10-14 23:17:08 +0800

    优化 417
    Signed-off-by: SaberCon <894876785@qq.com>

--- 401_500/417.py
+++ 401_500/417.py
@@ -4,27 +4,23 @@ class Solution:
             return []
         m = len(matrix)
         n = len(matrix[0])
-        a_set = set()
         p_set = set()
+        a_set = set()
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-        def dfs(x: int, y: int, height: int, is_atlantic: bool):
+        def dfs(x: int, y: int, height: int, o_set: set):
             if x < 0 or x >= m or y < 0 or y >= n:
                 return
-            if is_atlantic:
-                current_set = a_set
-            else:
-                current_set = p_set
-            if height > matrix[x][y] or (x, y) in current_set:
+            if height > matrix[x][y] or (x, y) in o_set:
                 return
-            current_set.add((x, y))
+            o_set.add((x, y))
             for direction in directions:
-                dfs(x + direction[0], y + direction[1], matrix[x][y], is_atlantic)
+                dfs(x + direction[0], y + direction[1], matrix[x][y], o_set)
 
         for i in range(n):
-            dfs(0, i, 0, False)
-            dfs(m - 1, i, 0, True)
+            dfs(0, i, 0, p_set)
+            dfs(m - 1, i, 0, a_set)
         for i in range(m):
-            dfs(i, 0, 0, False)
-            dfs(i, n - 1, 0, True)
+            dfs(i, 0, 0, p_set)
+            dfs(i, n - 1, 0, a_set)
         return [key for key in a_set & p_set]
