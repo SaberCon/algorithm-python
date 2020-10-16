3bba0f66c9d789bac8459bfe2effae6d56a78aba
SaberCon <894876785@qq.com>
2020-10-17 01:02:47 +0800

    优化 421
    Signed-off-by: SaberCon <894876785@qq.com>

--- 401_500/421.py
+++ 401_500/421.py
@@ -1,10 +1,25 @@
 class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
         length = len(bin(max(nums))) - 2
+
         max_xor = 0
-        for i in range(length)[::-1]:
-            max_xor <<= 1
-            curr_xor = max_xor | 1
-            prefixes = {num >> i for num in nums}
-            max_xor |= any(curr_xor ^ p in prefixes for p in prefixes)
+        trie = {}
+        for num in nums:
+            node = trie
+            xor_node = trie
+            curr_xor = 0
+            for i in range(length)[::-1]:
+                bit = (num >> i) & 1
+                if bit not in node:
+                    node[bit] = {}
+                node = node[bit]
+
+                toggled_bit = 1 - bit
+                curr_xor <<= 1
+                if toggled_bit in xor_node:
+                    curr_xor += 1
+                    xor_node = xor_node[toggled_bit]
+                else:
+                    xor_node = xor_node[bit]
+            max_xor = max(max_xor, curr_xor)
         return max_xor
