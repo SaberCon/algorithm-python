b2e90c16dd43af9b8e041ef1d3b8418efc8e5d71
SaberCon <894876785@qq.com>
2020-11-02 21:57:45 +0800

    440
    Signed-off-by: SaberCon <894876785@qq.com>

--- 401_500/440.py
+++ 401_500/440.py
@@ -1,2 +1,15 @@
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
+        def find_kth_number(prefix, k):
+            if k == 1:
+                return prefix
+            curr = prefix
+            next = curr + 1
+            count = 0
+            while curr <= n:
+                count += min(next, n + 1) - curr
+                curr *= 10
+                next *= 10
+            return find_kth_number(prefix + 1, k - count) if count < k else find_kth_number(prefix * 10, k - 1)
+
+        return find_kth_number(1, k)
