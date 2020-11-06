b6a3d90be439b0325c24ceed51cfc5f1a2845dfc
SaberCon <894876785@qq.com>
2020-11-06 08:34:12 +0800

    454

--- 401_500/454.py
+++ 401_500/454.py
@@ -1,3 +1,7 @@
 class Solution:
     def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
-        
+        AB = {}
+        for a in A:
+            for b in B:
+                AB[a + b] = AB.get(a + b, 0) + 1
+        return sum(AB[-(c + d)] for d in D for c in C if -(c + d) in AB)
