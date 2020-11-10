5634a70cbc1b8d5a222da2d2643bd0d12a3d0277
SaberCon <894876785@qq.com>
2020-11-10 17:18:38 +0800

    优化 467

--- 401_500/467.py
+++ 401_500/467.py
@@ -1,16 +1,11 @@
 class Solution:
     def findSubstringInWraproundString(self, p: str) -> int:
-        def get_ord(char, distance):
-            return (ord(char) - ord('a') + distance) % 26
-
-        p += 'E'
+        p = '_' + p
         counts = [0] * 26
-        start = 0
-        for i in range(0, len(p)):
-            if get_ord(p[start], i - start) == get_ord(p[i], 0):
-                continue
-            for j in range(0, min(26, i - start)):
-                index = get_ord(p[start], j)
-                counts[index] = max(counts[index], i - start - j)
-            start = i
+        length = 0
+        for i in range(1, len(p)):
+            index = ord(p[i]) - ord('a')
+            last_index = ord(p[i - 1]) - ord('a')
+            length = length + 1 if (index - last_index) % 26 == 1 else 1
+            counts[index] = max(counts[index], length)
         return sum(counts)
