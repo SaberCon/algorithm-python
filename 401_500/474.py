21e42d06d8375cf87216b34a884478bf3cad9645
SaberCon <894876785@qq.com>
2020-11-12 20:24:08 +0800

    474

--- 401_500/474.py
+++ 401_500/474.py
@@ -1,4 +1,11 @@
 class Solution:
-    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        counts = [(s.count('0'), s.count('1')) for s in strs]
-
+    def findMaxForm(self, strs: [str], m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for s in strs:
+            count_0 = s.count('0')
+            count_1 = len(s) - count_0
+            for i in range(m, -1, -1):
+                for j in range(n, -1, -1):
+                    if i - count_0 >= 0 and j - count_1 >= 0:
+                        dp[i][j] = max(dp[i][j], dp[i - count_0][j - count_1] + 1)
+        return dp[m][n]
