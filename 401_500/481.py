c91a5581d8b13ea5cbc42888e24a0aa862e74535
SaberCon <894876785@qq.com>
2020-11-14 10:44:12 +0800

    481

--- 401_500/481.py
+++ 401_500/481.py
@@ -1,2 +1,14 @@
 class Solution:
     def magicalString(self, n: int) -> int:
+        S = []
+        curr_index = 0
+        curr_val = True
+        while len(S) < n:
+            S.append(curr_val)
+            if not S[curr_index]:
+                S.append(curr_val)
+            curr_index += 1
+            curr_val = not curr_val
+        if len(S) > n:
+            S.pop()
+        return sum(S)
