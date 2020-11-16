9c0c6aaa237c2a9057e6933ec883622f44d6f14a
SaberCon <894876785@qq.com>
2020-11-16 09:20:06 +0800

    优化 486

--- 401_500/486.py
+++ 401_500/486.py
@@ -1,15 +1,7 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        cache = {}
-
-        def max_score(left, right):
-            if left > right:
-                return 0
-            if (left, right) in cache:
-                return cache[(left, right)]
-            left_score = nums[left] + min(max_score(left + 2, right), max_score(left + 1, right - 1))
-            right_score = nums[right] + min(max_score(left + 1, right - 1), max_score(left, right - 2))
-            cache[(left, right)] = max(left_score, right_score)
-            return cache[(left, right)]
-
-        return max_score(0, len(nums) - 1) >= (sum(nums) + 1) // 2
+        dp = nums.copy()
+        for i in range(len(nums) - 2, -1, -1):
+            for j in range(i + 1, len(nums)):
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1])
+        return dp[len(nums) - 1] >= 0
