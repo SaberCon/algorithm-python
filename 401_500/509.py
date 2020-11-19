d89b8944d9b2920bc30ebd4f7f585016aa48f29a
SaberCon <894876785@qq.com>
2020-11-19 18:55:57 +0800

    509

---
+++ 401_500/509.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, N: int) -> int:
+        if N < 2:
+            return N
+        a, b = 0, 1
+        for _ in range(N - 1):
+            a, b = b, a + b
+        return b
