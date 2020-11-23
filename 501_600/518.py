0bc71bf8b1c8fdfbdc437103fab12e634a4061bd
SaberCon <894876785@qq.com>
2020-11-23 20:01:09 +0800

    优化 518

--- 501_600/518.py
+++ 501_600/518.py
@@ -1,13 +1,8 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        amount_counts = {0: 1}
+        dp = [0] * (amount + 1)
+        dp[0] = 1
         for coin in coins:
-            new_counts = amount_counts.copy()
-            for coin_amount, count in amount_counts.items():
-                multi_coin = coin
-                while multi_coin + coin_amount <= amount:
-                    new_counts.setdefault(coin_amount + multi_coin, 0)
-                    new_counts[coin_amount + multi_coin] += amount_counts[coin_amount]
-                    multi_coin += coin
-            amount_counts = new_counts
-        return amount_counts[amount] if amount in amount_counts else 0
+            for x in range(coin, amount + 1):
+                dp[x] += dp[x - coin]
+        return dp[amount]
