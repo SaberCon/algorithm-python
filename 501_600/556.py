6d36e99611c25a07770e2ee06f3220cbe89a92a5
SaberCon <894876785@qq.com>
2020-11-26 18:57:53 +0800

    556

--- 501_600/556.py
+++ 501_600/556.py
@@ -1,4 +1,13 @@
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
-
-
+        nums = [int(c) for c in str(n)]
+        digits = []
+        for num in nums[::-1]:
+            if digits and digits[-1] > num:
+                for i, digit in enumerate(digits):
+                    if digit > num:
+                        digits[i] = num
+                        ans = int(''.join((str(d) for d in nums[:-len(digits) - 1] + [digit] + digits)))
+                        return ans if ans <= 2 ** 31 else -1
+            digits.append(num)
+        return -1
