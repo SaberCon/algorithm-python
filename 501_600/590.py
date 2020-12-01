dd703278b1899377dc5f41f3475fc35f7299c33e
SaberCon <894876785@qq.com>
2020-12-01 08:36:03 +0800

    590

---
+++ 501_600/590.py
@@ -0,0 +1,20 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        stack = [root]
+        ans = []
+        while stack:
+            node = stack.pop()
+            ans.append(node.val)
+            for child in node.children:
+                stack.append(child)
+        ans.reverse()
+        return ans
