9e816b43ca7af3a838057ad18196c33ef228c1a1
SaberCon <894876785@qq.com>
2020-12-02 19:03:16 +0800

    优化 587

--- 501_600/587.py
+++ 501_600/587.py
@@ -1,6 +1,3 @@
-from itertools import chain
-
-
 def diff(x1, y1, x2, y2, x3, y3):
     if x1 == x3:
         return 0
@@ -8,15 +5,15 @@ def diff(x1, y1, x2, y2, x3, y3):
 
 
 class Solution:
-    def outerTrees(self, points: [[int]]) -> [[int]]:
+    def outerTrees(self, points: List[List[int]]) -> List[List[int]]:
         top, bottom = [], []
         points.sort()
         for point in points:
             top.append(point)
-            bottom.append(point)
             while len(top) > 2 and diff(*top[-3], *top[-2], *top[-1]) > 0:
                 del top[-2]
+        for point in reversed(points):
+            bottom.append(point)
             while len(bottom) > 2 and diff(*bottom[-3], *bottom[-2], *bottom[-1]) < 0:
                 del bottom[-2]
-        return [[x, y] for (x, y) in
-                {(x, y) for (x, y) in chain(top, bottom, (p for p in points if p[0] == points[-1][0]))}]
+        return [[x, y] for (x, y) in {(x, y) for (x, y) in top + bottom}]
