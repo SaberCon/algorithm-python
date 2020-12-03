4c1f1999900478e9acfe76fdc66e8be93acded05
SaberCon <894876785@qq.com>
2020-12-03 10:51:30 +0800

    611

--- 601_700/611.py
+++ 601_700/611.py
@@ -1,2 +1,15 @@
 class Solution:
-    def triangleNumber(self, nums: List[int]) -> int:
+    def triangleNumber(self, nums: [int]) -> int:
+        nums.sort()
+        ans = 0
+        for i, num in enumerate(nums):
+            left, right = i + 1, i + 2
+            while right < len(nums):
+                if num + nums[left] > nums[right]:
+                    ans += right - left
+                    right += 1
+                else:
+                    left += 1
+                    if left == right:
+                        right += 1
+        return ans
