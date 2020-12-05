5b2d53cf4e68478adf7bcb6daba955fd51f7cb46
SaberCon <894876785@qq.com>
2020-12-05 15:02:37 +0800

    629
    Signed-off-by: SaberCon <894876785@qq.com>

--- 601_700/629.py
+++ 601_700/629.py
@@ -1,2 +1,13 @@
 class Solution:
     def kInversePairs(self, n: int, k: int) -> int:
+        mod = 10 ** 9 + 7
+        dp = [1] + [0] * k
+        for i in range(n):
+            new_dp = dp.copy()
+            for j in range(1, k + 1):
+                new_dp[j] += new_dp[j - 1]
+                if j > i:
+                    new_dp[j] -= dp[j - i - 1]
+                new_dp[j] %= mod
+            dp = new_dp
+        return dp[k]
