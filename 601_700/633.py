f19625c8317d58d9f5e34cf9706a5428f88a52b8
SaberCon <894876785@qq.com>
2020-12-06 19:31:31 +0800

    633
    Signed-off-by: SaberCon <894876785@qq.com>

--- 601_700/633.py
+++ 601_700/633.py
@@ -1,2 +1,6 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
+        for i in range(1, int(c ** 0.5) + 1):
+            if int((c - i ** 2) ** 0.5) ** 2 == c - i ** 2:
+                return True
+        return False
