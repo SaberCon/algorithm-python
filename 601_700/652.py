ba17e97d0c4887fd2165bda15d1ef5f36bf247ae
SaberCon <894876785@qq.com>
2020-12-09 18:50:33 +0800

    652

--- 601_700/652.py
+++ 601_700/652.py
@@ -1,4 +1,7 @@
 # Definition for a binary tree node.
+from collections import defaultdict
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -8,4 +11,18 @@ class TreeNode:
 
 class Solution:
     def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
+        trees = defaultdict()
+        trees.default_factory = trees.__len__
+        count = defaultdict(lambda: 0)
         ans = []
+
+        def lookup(node):
+            if node:
+                uid = trees[node.val, lookup(node.left), lookup(node.right)]
+                count[uid] += 1
+                if count[uid] == 2:
+                    ans.append(node)
+                return uid
+
+        lookup(root)
+        return ans
