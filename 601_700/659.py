685d2b543bcc77919f0efeda88e4180008d462e2
SaberCon <894876785@qq.com>
2020-12-11 13:05:01 +0800

    优化 659

--- 601_700/659.py
+++ 601_700/659.py
@@ -3,10 +3,20 @@ from collections import defaultdict
 
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
-        seqs = defaultdict(list)
+        counts, ends = defaultdict(lambda: 0), defaultdict(lambda: 0)
         for num in nums:
-            if seqs[num - 1]:
-                seqs[num].insert(0, seqs[num - 1].pop() + 1)
+            counts[num] += 1
+        for num in nums:
+            if not counts[num]:
+                continue
+            counts[num] -= 1
+            if ends[num - 1]:
+                ends[num - 1] -= 1
+                ends[num] += 1
             else:
-                seqs[num].append(1)
-        return all(all(s > 2 for s in seq) for seq in seqs.values())
+                if not counts[num + 1] or not counts[num + 2]:
+                    return False
+                counts[num + 1] -= 1
+                counts[num + 2] -= 1
+                ends[num + 2] += 1
+        return True
