4512638d363e42f46a0744e987d1bf550b5dbdfe
SaberCon <894876785@qq.com>
2020-12-14 18:54:04 +0800

    优化 672

--- 601_700/672.py
+++ 601_700/672.py
@@ -1,10 +1,5 @@
 class Solution:
     def flipLights(self, n: int, m: int) -> int:
-        n = min(n, 3)
-        if n == 0 or m == 0:
-            return 1
-        if m == 1:
-            return [2, 3, 4][n - 1]
-        if m == 2:
-            return [2, 4, 7][n - 1]
-        return [2, 4, 8][n - 1]
+        n = min(n, 3) - 1
+        m = min(m, 3)
+        return [[1, 1, 1], [2, 3, 4], [2, 4, 7], [2, 4, 8]][m][n]
