6ae435ba5c69357f6a52af8f23fb844684f246c4
SaberCon <894876785@qq.com>
2020-12-15 13:40:30 +0800

    优化 675

--- 601_700/675.py
+++ 601_700/675.py
@@ -1,30 +1,33 @@
-import heapq
+import collections
 
 
 class Solution:
     def cutOffTree(self, forest: List[List[int]]) -> int:
         R, C = len(forest), len(forest[0])
 
-        def astar(sr, sc, tr, tc):
-            heap = [(0, 0, sr, sc)]
+        def hadlocks(sr, sc, tr, tc):
             seen = set()
-            while heap:
-                f, g, r, c = heapq.heappop(heap)
-                if r == tr and c == tc:
-                    return g
+            deque = collections.deque([(0, sr, sc)])
+            while deque:
+                detours, r, c = deque.popleft()
                 if (r, c) in seen:
                     continue
                 seen.add((r, c))
-                for nr, nc in ((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)):
+                if r == tr and c == tc:
+                    return abs(sr - tr) + abs(sc - tc) + 2 * detours
+                for nr, nc, closer in ((r - 1, c, r > tr), (r + 1, c, r < tr),
+                                       (r, c - 1, c > tc), (r, c + 1, c < tc)):
                     if 0 <= nr < R and 0 <= nc < C and forest[nr][nc]:
-                        ncost = g + 1 + abs(nr - tr) + abs(nc - tc)
-                        heapq.heappush(heap, (ncost, g + 1, nr, nc))
+                        if closer:
+                            deque.appendleft((detours, nr, nc))
+                        else:
+                            deque.append((detours + 1, nr, nc))
             return -1
 
         trees = sorted((v, r, c) for r, row in enumerate(forest) for c, v in enumerate(row) if v > 1)
         sr = sc = ans = 0
         for _, tr, tc in trees:
-            d = astar(sr, sc, tr, tc)
+            d = hadlocks(sr, sc, tr, tc)
             if d < 0:
                 return -1
             ans += d
