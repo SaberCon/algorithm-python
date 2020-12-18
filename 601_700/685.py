d09cda16c4cc8e17a569ff3e46f5387785e678d5
SaberCon <894876785@qq.com>
2020-12-18 09:59:49 +0800

    685

--- 601_700/685.py
+++ 601_700/685.py
@@ -1,2 +1,31 @@
 class Solution:
     def findRedundantDirectedConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = [i for i in range(len(edges) + 1)]
+
+        def find_root(n):
+            while uf[n] != n:
+                n = uf[n]
+            return n
+
+        invalid = first_invalid = second_invalid = None
+        children = set()
+        for f, c in edges:
+            if c in children:
+                invalid = c
+                break
+            children.add(c)
+
+        for i, (f, c) in enumerate(edges):
+            if c == invalid:
+                if not first_invalid:
+                    first_invalid = (f, c)
+                else:
+                    second_invalid = (f, c)
+                continue
+            rf, rc = find_root(f), find_root(c)
+            if rf == rc:
+                return f, c
+            uf[rc] = rf
+        if find_root(first_invalid[0]) == find_root(first_invalid[1]):
+            return first_invalid
+        return second_invalid
