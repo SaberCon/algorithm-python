c73eecc0abdd0bd6fe5966b47812045d40853237
SaberCon <894876785@qq.com>
2020-12-21 10:06:06 +0800

    696

--- 601_700/696.py
+++ 601_700/696.py
@@ -1,2 +1,13 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
+        stack = []
+        count = 0
+        for curr, prev in zip(s + 'E', 'S' + s):
+            if curr != prev:
+                stack.append(count)
+                count = 0
+            count += 1
+        ans = 0
+        while len(stack) > 1:
+            ans += min(stack.pop(), stack[-1])
+        return ans
