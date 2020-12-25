ff69e463639436397235d787d13a2e3d4e8e7ff6
SaberCon <894876785@qq.com>
2020-12-25 09:38:26 +0800

    719

--- 701_800/719.py
+++ 701_800/719.py
@@ -1,4 +1,17 @@
 class Solution:
-    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+    def smallestDistancePair(self, nums: [int], k: int) -> int:
         nums.sort()
-        distances = [n2 - n1 for n1, n2 in zip(nums, nums[1:])]
+        l, r = min(n2 - n1 for n1, n2 in zip(nums, nums[1:])), nums[-1] - nums[0]
+        while l < r:
+            mid = (l + r) // 2
+            count = 0
+            e = 1
+            for s in range(len(nums) - 1):
+                while e < len(nums) and nums[e] - nums[s] <= mid:
+                    e += 1
+                count += e - s - 1
+            if count < k:
+                l = mid + 1
+            else:
+                r = mid
+        return l
