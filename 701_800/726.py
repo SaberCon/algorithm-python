24d797e436b2c7aac6d26ae52162cb6be4e52687
SaberCon <894876785@qq.com>
2020-12-30 10:03:25 +0800

    726

--- 701_800/726.py
+++ 701_800/726.py
@@ -1,5 +1,27 @@
+from collections import Counter
+
+
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
-        counts = {}
-        
-        return ''.join(name + (str(count) if count > 1 else '') for name, count in sorted(counts.items()))
+        atoms = []
+        for c in formula:
+            if c == '(' or c == ')' or c.isupper():
+                atoms.append([c, 0])
+            if c.islower():
+                atoms[-1][0] += c
+            if c.isdigit():
+                atoms[-1][1] = 10 * atoms[-1][1] + int(c)
+        stack = [Counter()]
+        for atom, count in atoms:
+            if count == 0:
+                count = 1
+            if atom == '(':
+                stack.append(Counter())
+            elif atom == ')':
+                counter = stack.pop()
+                for k in counter:
+                    counter[k] *= count
+                stack[-1] += counter
+            else:
+                stack[-1][atom] += count
+        return ''.join(name + (str(count) if count > 1 else '') for name, count in sorted(stack.pop().items()))
