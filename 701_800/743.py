2b143319865c069d1de058c381cf63e7315ee2a1
SaberCon <894876785@qq.com>
2021-01-05 10:59:01 +0800

    743

--- 701_800/743.py
+++ 701_800/743.py
@@ -1,2 +1,21 @@
+import heapq
+from collections import defaultdict
+
+
 class Solution:
     def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        delays = defaultdict(list)
+        for source, target, time in times:
+            delays[source].append((target, time))
+        visited = set()
+        heap = [(0, K)]
+        while heap:
+            delay, target = heapq.heappop(heap)
+            if target in visited:
+                continue
+            visited.add(target)
+            if len(visited) == N:
+                return delay
+            for nex, time in delays[target]:
+                heapq.heappush(heap, (delay + time, nex))
+        return -1
