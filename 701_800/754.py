5268aef1559d1b81cb473da9caa8941e4e19b72b
SaberCon <894876785@qq.com>
2021-01-08 09:30:10 +0800

    754

--- 701_800/754.py
+++ 701_800/754.py
@@ -1,2 +1,9 @@
 class Solution:
     def reachNumber(self, target: int) -> int:
+        target = abs(target)
+        i = int((target * 2) ** 0.5)
+        while True:
+            total = i * (i + 1) // 2
+            if total >= target and (total % 2) == (target % 2):
+                return i
+            i += 1
