386c64acef6131dfe6ace2251c81bbfc1123fb81
SaberCon <894876785@qq.com>
2021-01-11 11:40:47 +0800

    766

---
+++ 701_800/766.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        prev = matrix[0][1:]
+        for row in matrix:
+            if row[1:] != prev:
+                return False
+            prev = row[:-1]
+        return True
