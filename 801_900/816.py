580cd10a364e1ce2ff3fbbbd7c606a3c6a4cd0ac
SaberCon <894876785@qq.com>
2021-01-31 09:32:37 +0800

    优化 816
    Signed-off-by: SaberCon <894876785@qq.com>

--- 801_900/816.py
+++ 801_900/816.py
@@ -3,14 +3,15 @@ from itertools import product
 
 class Solution:
     def ambiguousCoordinates(self, S: str) -> List[str]:
-        def make(frag):
-            N = len(frag)
-            for d in range(1, N + 1):
-                left = frag[:d]
-                right = frag[d:]
-                if ((not left.startswith('0') or left == '0')
-                        and (not right.endswith('0'))):
-                    yield left + ('.' if d != N else '') + right
+        def make_decimal(s):
+            if 1 < len(s) and s.startswith('0') and s.endswith('0'):
+                return []
+            if s.endswith('0'):
+                return [s]
+            if s.startswith('0'):
+                return ['0.' + s[1:]]
+            return [s[:i] + '.' + s[i:] if s[i:] else s[:i] for i in range(1, len(s) + 1)]
 
         S = S[1:-1]
-        return ["({}, {})".format(*cand) for i in range(1, len(S)) for cand in product(make(S[:i]), make(S[i:]))]
+        return ['({}, {})'.format(l, r) for i in range(1, len(S))
+                for l, r in product(make_decimal(S[:i]), make_decimal(S[i:]))]
