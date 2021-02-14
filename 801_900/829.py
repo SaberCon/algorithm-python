65786b6f58c55fdb5e4e5b1742c8377a2ceba6e6
SaberCon <894876785@qq.com>
2021-02-14 11:01:48 +0800

    829
    Signed-off-by: SaberCon <894876785@qq.com>

--- 801_900/829.py
+++ 801_900/829.py
@@ -1,3 +1,4 @@
 class Solution:
     def consecutiveNumbersSum(self, N: int) -> int:
-# N/n - (n-1)/2 == x        2N > n(n-1)
+        return sum(1 for i in range(1, int((2 * N) ** 0.5) + 1)
+                   if (i % 2 and not N % i) or (not i % 2 and not (N - i / 2) % i))
