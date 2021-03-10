77c1d6f85872401d1e9f197a5f7d3afeeeb655f3
SaberCon <894876785@qq.com>
2021-03-10 23:18:16 +0800

    855
    Signed-off-by: SaberCon <894876785@qq.com>

--- 801_900/855.py
+++ 801_900/855.py
@@ -1,10 +1,30 @@
+from bisect import bisect_left
+
+from bisect import insort_left
+
+
 class ExamRoom:
 
     def __init__(self, N: int):
-        pass
+        self.seats = []
+        self.size = N
 
     def seat(self) -> int:
-        pass
+        if not self.seats:
+            self.seats.append(0)
+            return 0
+        pos = distance = 0
+        if self.seats[0] != 0:
+            distance = self.seats[0]
+            pos = 0
+        for i in range(len(self.seats) - 1):
+            if (self.seats[i + 1] - self.seats[i]) // 2 > distance:
+                distance = (self.seats[i + 1] - self.seats[i]) // 2
+                pos = self.seats[i] + distance
+        if self.seats[-1] != self.size - 1 and self.size - 1 - self.seats[-1] > distance:
+            pos = self.size - 1
+        insort_left(self.seats, pos)
+        return pos
 
     def leave(self, p: int) -> None:
-        pass
+        self.seats.pop(bisect_left(self.seats, p))
