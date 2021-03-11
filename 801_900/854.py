364c40298bdf1a5b0c2b9f68b726193b4288e7f4
SaberCon <894876785@qq.com>
2021-03-11 23:36:20 +0800

    854
    Signed-off-by: SaberCon <894876785@qq.com>

--- 801_900/854.py
+++ 801_900/854.py
@@ -1,2 +1,19 @@
-class Solution:
+import collections
+
+
+class Solution(object):
     def kSimilarity(self, A: str, B: str) -> int:
+        def neighbors(S):
+            i = next(p for p, (s, b) in enumerate(zip(S, B)) if s != b)
+            return (S[:i] + S[j] + S[i + 1:j] + S[i] + S[j + 1:] for j in range(i + 1, len(S)) if S[j] == B[i])
+
+        queue = collections.deque([(A, 0)])
+        seen = {A}
+        while True:
+            S, count = queue.popleft()
+            if S == B:
+                return count
+            for N in neighbors(S):
+                if N not in seen:
+                    seen.add(N)
+                    queue.append((N, count + 1))
