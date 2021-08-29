39883ee93d1d5f5a0cbe3f8e1263d3731523d759
SaberCon <894876785@qq.com>
2021-08-30 07:59:14 +0800

    improve 871

--- 801_900/871.py
+++ 801_900/871.py
@@ -1,17 +1,15 @@
+import heapq
+
+
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
-        dp = {0: startFuel}
-        current = 0
-        for p, f in stations + [(target, 0)]:
-            new_dp = {}
-            for stops, fuel in dp.items():
-                cost = p - current
-                if fuel < cost:
-                    continue
-                if stops not in new_dp or fuel - cost > new_dp[stops]:
-                    new_dp[stops] = fuel - cost
-                if (stops + 1) not in new_dp or fuel - cost + f > new_dp[stops + 1]:
-                    new_dp[stops + 1] = fuel - cost + f
-            dp = new_dp
-            current = p
-        return min(dp.keys()) if dp else -1
+        heap = []
+        stops = 0
+        for pos, fuel in stations + [(target, 0)]:
+            while startFuel < pos:
+                if not heap:
+                    return -1
+                startFuel -= heapq.heappop(heap)
+                stops += 1
+            heapq.heappush(heap, -fuel)
+        return stops
