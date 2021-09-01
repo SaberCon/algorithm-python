e40e3bfec99a16954a0691a85bcf58db0e329333
SaberCon <894876785@qq.com>
2021-09-01 23:39:55 +0800

    improve 873

--- 801_900/873.py
+++ 801_900/873.py
@@ -1,11 +1,17 @@
+from collections import defaultdict
+
+
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
-        seq_count = {}
-        for i in range(1, len(arr)):
+        counts = defaultdict(lambda: 2)
+        visited = set()
+        ans = 0
+        for i, num in enumerate(arr):
             for j in range(i):
-                if (arr[j], arr[i] - arr[j]) in seq_count:
-                    seq_count[(arr[i] - arr[j], arr[i])] = max(3, seq_count[(arr[j], arr[i] - arr[j])] + 1)
-                    del seq_count[(arr[j], arr[i] - arr[j])]
-                if (arr[j], arr[i]) not in seq_count:
-                    seq_count[(arr[j], arr[i])] = 0
-        return max(seq_count.values())
+                if arr[j] >= (num + 1) // 2:
+                    break
+                if num - arr[j] in visited:
+                    counts[(num - arr[j], num)] = counts[(arr[j], num - arr[j])] + 1
+                    ans = max(ans, counts[(num - arr[j], num)])
+            visited.add(num)
+        return ans
