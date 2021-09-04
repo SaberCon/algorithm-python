71b03b93a636edffd1ff9367127e87ef8b99bb51
SaberCon <894876785@qq.com>
2021-09-04 09:28:25 +0800

    876

---
+++ 801_900/876.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        tortoise = hare = head
+        while hare and hare.next:
+            hare = hare.next.next
+            tortoise = tortoise.next
+        return tortoise
