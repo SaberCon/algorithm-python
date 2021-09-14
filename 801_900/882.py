c5014a379b8ff115445fa12da58c536a19b253a5
SaberCon <894876785@qq.com>
2021-09-14 08:46:49 +0800

    882

--- 801_900/882.py
+++ 801_900/882.py
@@ -1,3 +1,30 @@
+import heapq
+
+
 class Solution:
     def reachableNodes(self, edges: List[List[int]], maxMoves: int, n: int) -> int:
-        
+        connection = [[] for _ in range(n)]
+        for n1, n2, cnt in edges:
+            connection[n1].append((n2, cnt))
+            connection[n2].append((n1, cnt))
+        visited = {}
+        ans = 0
+        queue = [(-maxMoves - 1, 0)]
+        while queue:
+            moves, i = heapq.heappop(queue)
+            if i in visited:
+                continue
+            moves = -moves - 1
+            ans += 1
+            visited[i] = {}
+            for j, cnt in connection[i]:
+                if j in visited:
+                    ans += min(moves, cnt - visited[j][i])
+                elif moves > cnt:
+                    ans += cnt
+                    visited[i][j] = cnt
+                    heapq.heappush(queue, (cnt - moves, j))
+                else:
+                    ans += moves
+                    visited[i][j] = moves
+        return ans
