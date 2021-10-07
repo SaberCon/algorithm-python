d07efce013c3cb6fb0b923ab12ac34aa3b362d3b
SaberCon <894876785@qq.com>
2021-10-07 08:48:38 +0800

    improve 906

--- 901_1000/906.py
+++ 901_1000/906.py
@@ -10,26 +10,26 @@ class Solution:
         def build_palindrome(half, mid=''):
             return int(half + mid + half[::-1])
 
-        def build_one_zero(l, m):
+        memo = {}
+
+        def build_10(l, m):
             if m < 0:
                 return []
             if l == 0:
                 return ['']
-
-            group = ['0' + s for s in build_one_zero(l - 1, m)]
-            if m >= 2:
-                group += ['1' + s for s in build_one_zero(l - 1, m - 2)]
-            return group
+            if (l, m) not in memo:
+                memo[l, m] = ['0' + s for s in build_10(l - 1, m)] + ['1' + s for s in build_10(l - 1, m - 2)]
+            return memo[l, m]
 
         for i in range(max(2, len(str(left))), len(str(right)) + 1):
             sps = []
             if i % 2:
                 for j in range(0, 3):
-                    sps += [build_palindrome('1' + oz, str(j)) for oz in build_one_zero(i // 2 - 1, 7 - j * j)]
-                    sps += [build_palindrome('2' + oz, str(j)) for oz in build_one_zero(i // 2 - 1, 1 - j * j)]
+                    sps += [build_palindrome('1' + oz, str(j)) for oz in build_10(i // 2 - 1, 7 - j * j)]
+                    sps += [build_palindrome('2' + oz, str(j)) for oz in build_10(i // 2 - 1, 1 - j * j)]
             else:
-                sps += [build_palindrome('1' + oz) for oz in build_one_zero(i // 2 - 1, 7)]
-                sps += [build_palindrome('2' + oz) for oz in build_one_zero(i // 2 - 1, 1)]
+                sps += [build_palindrome('1' + oz) for oz in build_10(i // 2 - 1, 7)]
+                sps += [build_palindrome('2' + oz) for oz in build_10(i // 2 - 1, 1)]
 
             ans += sum(1 for sp in sps if left <= sp <= right)
         return ans
