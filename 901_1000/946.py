bf72ddb4d138432b49934cfa362d6c37f1eb8de2
SaberCon <894876785@qq.com>
2021-11-07 21:39:59 +0800

    improve 946

--- 901_1000/946.py
+++ 901_1000/946.py
@@ -1,14 +1,10 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        indices = {n: i for i, n in enumerate(popped)}
-
-        def check(pushed_i, popped_i, size):
-            if size == 0:
-                return True
-            p = indices[pushed[pushed_i]]
-            if p < popped_i or p >= popped_i + size:
-                return False
-            left, right = p - popped_i, size - (p - popped_i) - 1
-            return check(pushed_i + 1, popped_i, left) and check(pushed_i + left + 1, p + 1, right)
-
-        return check(0, 0, len(pushed))
+        popped = popped[::-1]
+        stack = []
+        for num in pushed:
+            stack.append(num)
+            while stack and popped[-1] == stack[-1]:
+                popped.pop()
+                stack.pop()
+        return not stack
