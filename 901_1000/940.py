72765db8c451317741de3158db5879abf6fa3b03
SaberCon <894876785@qq.com>
2021-11-07 22:50:21 +0800

    940

--- 901_1000/940.py
+++ 901_1000/940.py
@@ -1,2 +1,9 @@
 class Solution:
     def distinctSubseqII(self, s: str) -> int:
+        MOD = 10 ** 9 + 7
+        dp = [1] * (len(s) + 1)
+        counts = [0] * 26
+        for i, c in enumerate(s):
+            dp[i + 1] = (2 * dp[i] - counts[ord(c) - ord('a')]) % MOD
+            counts[ord(c) - ord('a')] = dp[i]
+        return (dp[-1] - 1) % MOD
