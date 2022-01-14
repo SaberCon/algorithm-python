a23da297e3bdd609ad9f90c1df4885717b0a96d1
SaberCon <894876785@qq.com>
2022-01-14 14:47:53 +0800

    1012

--- 1001_1100/1012.py
+++ 1001_1100/1012.py
@@ -1,7 +1,23 @@
+from math import factorial
+
+
 class Solution:
     def numDupDigitsAtMostN(self, n: int) -> int:
-        nums = [int(c) for c in reversed(str(n))]
-        ans = 0
-        for i, num in nums:
-            ans +=
+        def f(count, size):
+            return factorial(count) // factorial(count - size)
+
+        nums = [int(c) for c in str(n)]
+        N = len(nums)
+        ans = sum(9 * f(9, i) for i in range(N - 1))
+        seen = [False] * 10
+        for i, num in enumerate(nums):
+            for j in range(num):
+                if seen[j] or (i == j == 0):
+                    continue
+                ans += f(9 - i, N - i - 1)
+            if seen[num]:
+                break
+            seen[num] = True
+        else:
+            ans += 1
         return n - ans
