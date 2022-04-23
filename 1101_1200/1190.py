433faf1a54310cc625d7ed043d5b26df0df2d7ec
SaberCon <894876785@qq.com>
2022-04-23 11:18:54 +0800

    Improve 1190

--- 1101_1200/1190.py
+++ 1101_1200/1190.py
@@ -1,12 +1,21 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        stack = ['']
-        for char in s:
-            if char == '(':
-                stack.append('')
-            elif char == ')':
-                letters = stack.pop()[::-1]
-                stack[-1] += letters
+        stack = []
+        pairs = {}
+        for i, c in enumerate(s):
+            if c == '(':
+                stack.append(i)
+            if c == ')':
+                j = stack.pop()
+                pairs[i], pairs[j] = j, i
+        ans = []
+        i = 0
+        d = 1
+        while i < len(s):
+            if s[i] == '(' or s[i] == ')':
+                i = pairs[i] - d
+                d = -d
             else:
-                stack[-1] += char
-        return stack[0]
+                ans.append(s[i])
+                i += d
+        return ''.join(ans)
