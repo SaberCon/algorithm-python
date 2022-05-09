17301ad3bca4b9d01aafb107ba1f55da7a58baee
SaberCon <894876785@qq.com>
2022-05-10 00:20:41 +0800

    1222

---
+++ 1201_1300/1222.py
@@ -0,0 +1,17 @@
+class Solution:
+    def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
+        queens = {(i, j) for i, j in queens}
+        ans = []
+        ki, kj = king
+        for di in range(-1, 2):
+            for dj in range(-1, 2):
+                if di == 0 and dj == 0:
+                    continue
+                for t in range(1, 8):
+                    i, j = ki + di * t, kj + dj * t
+                    if not (0 <= i < 8 and 0 <= j < 8):
+                        break
+                    if (i, j) in queens:
+                        ans.append([i, j])
+                        break
+        return ans
