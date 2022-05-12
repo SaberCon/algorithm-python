fab7741992d53f26f10946448bddd126b55b522d
SaberCon <894876785@qq.com>
2022-05-12 09:04:45 +0800

    Improve 1227

--- 1201_1300/1227.py
+++ 1201_1300/1227.py
@@ -1,3 +1,7 @@
 class Solution:
     def nthPersonGetsNthSeat(self, n: int) -> float:
-        return 0.5 if n > 1 else 1.0
+        dp = 0
+        for i in range(2, n):
+            prob = 1 / (i * i)
+            dp = prob + (1 - prob) * dp
+        return 1 / n + (1 - 1 / n) * dp
