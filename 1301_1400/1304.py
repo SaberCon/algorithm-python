db31333c25f3d0fb2fa7c13a2b8714a7e11f91c4
SaberCon <894876785@qq.com>
2022-06-27 07:28:25 +0800

    1304

---
+++ 1301_1400/1304.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        if n == 2:
+            return [1, -1]
+        return [i for i in range(n - 1)] + [-(n - 1) * (n - 2) // 2]
