9ab05710529ddb976ee584bcdf0d08a5f2971629
SaberCon <894876785@qq.com>
2022-09-11 07:45:05 +0800

    Improve 1404

--- 1401_1500/1404.py
+++ 1401_1500/1404.py
@@ -1,11 +1,8 @@
 class Solution:
     def numSteps(self, s: str) -> int:
-        carry = ans = 0
+        carry = False
+        ans = 0
         for b in s[:0:-1]:
-            carry += b == '1'
-            if carry % 2:
-                carry += 1
-                ans += 1
-            carry //= 2
-            ans += 1
+            ans += 1 + (carry ^ (b == '1'))
+            carry |= b == '1'
         return ans + carry
