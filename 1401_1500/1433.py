941a3cdaaf28dc0c3887e9d81ef1de76baa18771
SaberCon <894876785@qq.com>
2022-09-27 00:04:56 +0800

    1433

---
+++ 1401_1500/1433.py
@@ -0,0 +1,4 @@
+class Solution:
+    def checkIfCanBreak(self, s1: str, s2: str) -> bool:
+        s1, s2 = sorted(s1), sorted(s2)
+        return all(c1 >= c2 for c1, c2 in zip(s1, s2)) or all(c1 <= c2 for c1, c2 in zip(s1, s2))
