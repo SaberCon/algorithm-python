34b8b4b6a4676cec3ff3a1a409f6178263b59d24
SaberCon <894876785@qq.com>
2022-10-06 10:30:44 +0800

    1447

---
+++ 1401_1500/1447.py
@@ -0,0 +1,7 @@
+from math import gcd
+from typing import List
+
+
+class Solution:
+    def simplifiedFractions(self, n: int) -> List[str]:
+        return [f"{i}/{j}" for i in range(1, n) for j in range(i + 1, n + 1) if gcd(i, j) == 1]
