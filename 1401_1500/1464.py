9a540cf784764ecc0f0a403317bcbaa8ea520a49
SaberCon <894876785@qq.com>
2022-10-17 07:34:24 +0800

    1464

---
+++ 1401_1500/1464.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        a = b = 0
+        for num in nums:
+            if num > a:
+                a, b = num, a
+            elif num > b:
+                b = num
+        return (a - 1) * (b - 1)
